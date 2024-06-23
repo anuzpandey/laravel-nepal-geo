e25c8266084cc59ac5ed6062df517639fd5f77ea
AnuzPandey <anuzbvbmaniac123@gmail.com>
2024-06-23 23:56:49 +0545

    :recycle: refac: Update Main Seeder FileName.

--- database/seeders/NepalGeoSeeder.php
+++ database/seeders/NepalGeoSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class NepaliStateDistrictCitySeeder extends Seeder
+class NepalGeoSeeder extends Seeder
 {
     public function run(): void
     {
